fb2cf745c53e2c6104c30f95ce1be48c26d4077d
Oleg Titov <oleggtitoff@gmail.com>
2020-01-09 01:46:29 +0200

    In ProductInCheckRepository added findByCheck (all tests are green)

--- src/main/java/ua/training/cashregister/repository/ProductInCheckRepository.java
+++ src/main/java/ua/training/cashregister/repository/ProductInCheckRepository.java
@@ -2,9 +2,13 @@ package ua.training.cashregister.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ua.training.cashregister.entity.Check;
 import ua.training.cashregister.entity.ProductInCheck;
+
+import java.util.List;
 
 @Repository
 public interface ProductInCheckRepository
         extends JpaRepository<ProductInCheck, Long> {
+    List<ProductInCheck> findByCheck(Check check);
 }
