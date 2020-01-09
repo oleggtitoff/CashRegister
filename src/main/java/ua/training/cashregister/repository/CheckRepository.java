b56f86d19c725595e64b7b7eba93ca58b921bac3
Oleg Titov <oleggtitoff@gmail.com>
2020-01-09 07:20:01 +0200

    In CheckRepository added findAllByIsInFiscalMemoryFalse (all tests are green)

--- src/main/java/ua/training/cashregister/repository/CheckRepository.java
+++ src/main/java/ua/training/cashregister/repository/CheckRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ua.training.cashregister.entity.Check;
 
+import java.util.List;
+
 @Repository
 public interface CheckRepository extends JpaRepository<Check, Long> {
+    List<Check> findAllByIsInFiscalMemoryFalse();
 }
