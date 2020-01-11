c48865df2030c83ac59f37825fd6949ee5a982f6
Oleg Titov <oleggtitoff@gmail.com>
2020-01-11 18:24:51 +0200

    In admin/products.html added table with products (all tests are green)

--- src/main/java/ua/training/cashregister/controller/AdminPagesController.java
+++ src/main/java/ua/training/cashregister/controller/AdminPagesController.java
@@ -1,19 +1,30 @@
 package ua.training.cashregister.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.training.cashregister.service.ProductService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminPagesController {
+    private ProductService productService;
+
+    @Autowired
+    public AdminPagesController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping({"/", "/index"})
     public String getIndexPage() {
         return "admin/index";
     }
 
     @GetMapping("/products")
-    public String getProductsPage() {
+    public String getProductsPage(Model model) {
+        model.addAttribute("products", productService.getAllProducts().getProducts());
         return "admin/products";
     }
 
