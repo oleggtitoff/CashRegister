e52b3dc9ba0c21d467ef67e82758816af361fe62
Oleg Titov <oleggtitoff@gmail.com>
2020-01-11 16:50:57 +0200

    In AdminPagesController added GetMapping (all tests are green)

--- src/main/java/ua/training/cashregister/controller/AdminPagesController.java
+++ src/main/java/ua/training/cashregister/controller/AdminPagesController.java
@@ -1,10 +1,39 @@
 package ua.training.cashregister.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminPagesController {
-    //TODO
+    @GetMapping({"/", "/index"})
+    public String getIndexPage() {
+        return "admin/index";
+    }
+
+    @GetMapping("/products")
+    public String getProductsPage() {
+        return "admin/products";
+    }
+
+    @GetMapping("/new-product")
+    public String getNewProductPage() {
+        return "admin/new-product";
+    }
+
+    @GetMapping({"/statistics", "/statistics/index"})
+    public String getStatisticsIndexPage() {
+        return "admin/statistics/index";
+    }
+
+    @GetMapping("/statistics/report")
+    public String getStatisticsReportPage() {
+        return "admin/statistics/report";
+    }
+
+    @GetMapping("/statistics/check")
+    public String getStatisticsCheckPage() {
+        return "admin/statistics/check";
+    }
 }
