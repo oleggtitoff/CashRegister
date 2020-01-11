7da68eccb7ba9b8d1a9003feeb4a7ac43d6bd606
Oleg Titov <oleggtitoff@gmail.com>
2020-01-11 15:22:17 +0200

    Added empty AdminPagesController (all tests are green)

---
+++ src/main/java/ua/training/cashregister/controller/AdminPagesController.java
@@ -0,0 +1,10 @@
+package ua.training.cashregister.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminPagesController {
+    //TODO
+}
