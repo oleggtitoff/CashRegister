46f9f41eba4c2056afa8825f57a79d75c1d44377
Oleg Titov <oleggtitoff@gmail.com>
2020-01-12 21:36:37 +0200

    In Product added validation annotations (all tests are green)

--- src/main/java/ua/training/cashregister/entity/Product.java
+++ src/main/java/ua/training/cashregister/entity/Product.java
@@ -3,6 +3,9 @@ package ua.training.cashregister.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -22,9 +25,11 @@ public class Product {
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
+    @NotBlank(message = "Name cannot be blank")
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
+    @Positive(message = "Price must be positive")
     @Column(name = "price", nullable = false)
     private BigDecimal price;
 
@@ -32,10 +37,12 @@ public class Product {
     @Enumerated(EnumType.STRING)
     private ProductType productType;
 
+    @PositiveOrZero(message = "Total quantity cannot be negative")
     @Builder.Default
     @Column(name = "total_quantity")
     private BigInteger totalQuantity = BigInteger.ZERO;
 
+    @PositiveOrZero(message = "Total mass cannot be negative")
     @Builder.Default
     @Column(name = "total_mass")
     private BigDecimal totalMass = BigDecimal.ZERO;
