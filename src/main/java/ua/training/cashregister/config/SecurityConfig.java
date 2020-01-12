33ae3d0cb2a28f2327197b51c7a2188243ebd6a5
Oleg Titov <oleggtitoff@gmail.com>
2020-01-12 23:16:08 +0200

    Added SecurityConfig.java (all tests are green)

---
+++ src/main/java/ua/training/cashregister/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package ua.training.cashregister.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+}
