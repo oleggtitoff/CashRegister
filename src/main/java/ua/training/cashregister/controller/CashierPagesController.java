b9b6b986208205a1cf405156abc260d28bb38f66
Oleg Titov <oleggtitoff@gmail.com>
2020-01-12 21:44:39 +0200

    In CashierPagesController added getIndexPage method (all tests are green)

--- src/main/java/ua/training/cashregister/controller/CashierPagesController.java
+++ src/main/java/ua/training/cashregister/controller/CashierPagesController.java
@@ -1,10 +1,14 @@
 package ua.training.cashregister.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/cashier")
 public class CashierPagesController {
-    //TODO
+    @GetMapping({"/", "/index"})
+    public String getIndexPage() {
+        return "cashier/index";
+    }
 }
