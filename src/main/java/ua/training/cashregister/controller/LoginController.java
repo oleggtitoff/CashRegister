9d8f147f60597b17ae04453afe1be04c0fd3f7f3
Oleg Titov <oleggtitoff@gmail.com>
2020-01-13 02:02:13 +0200

    Added login.html page (all tests are green)

---
+++ src/main/java/ua/training/cashregister/controller/LoginController.java
@@ -0,0 +1,19 @@
+package ua.training.cashregister.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    public String getLogin(
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout,
+            Model model) {
+        model.addAttribute("error", error);
+        model.addAttribute("logout", logout);
+        return "login";
+    }
+}
