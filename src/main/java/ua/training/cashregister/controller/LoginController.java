c50f3cbd6c009b1c436ca82853c63497b0690fcd
Oleg Titov <oleggtitoff@gmail.com>
2020-01-14 01:04:51 +0200

    In LoginController added getSuccessPage method (all tests are green)

--- src/main/java/ua/training/cashregister/controller/LoginController.java
+++ src/main/java/ua/training/cashregister/controller/LoginController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class LoginController {
     @RequestMapping("/login")
@@ -15,5 +17,13 @@ public class LoginController {
         model.addAttribute("error", error);
         model.addAttribute("logout", logout);
         return "login";
+    }
+
+    @RequestMapping("/default-success")
+    public String getSuccessPage(HttpServletRequest request) {
+        if (request.isUserInRole("ROLE_ADMIN")) {
+            return "redirect:/admin/index";
+        }
+        return "redirect:/cashier/index";
     }
 }
